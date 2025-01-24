d1be4c332a073fd83b84d1f8cac3e9af4bef7cf0
cristian <cchura.cpc@gmail.com>
2025-01-23 21:09:00 -0400

    CRUD Usuarios

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -23,3 +24,6 @@ Route::prefix('/v1/auth')->group(function(){
     });
 
 });
+
+// CRUD Usuarios
+Route::apiResource("usuario", UsuarioController::class);
