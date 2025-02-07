1a56a2637f48feb6d5cbd9fe5bbe2519d15e0ae1
cristian <cchura.cpc@gmail.com>
2025-02-06 21:03:08 -0400

    CRUDs categoria y Producto

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\UsuarioController;
 
 Route::get('/user', function (Request $request) {
@@ -25,5 +29,26 @@ Route::prefix('/v1/auth')->group(function(){
 
 });
 
-// CRUD Usuarios
-Route::apiResource("usuario", UsuarioController::class);
+
+Route::get('categoria-publica', [CategoriaController::class, 'listaCategoriaPublica']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // CRUD Usuarios
+    Route::apiResource("usuario", UsuarioController::class);
+    Route::apiResource("categoria", CategoriaController::class);
+    Route::apiResource("producto", ProductoController::class);
+    Route::apiResource("cliente", ClienteController::class);
+    Route::apiResource("pedido", PedidoController::class);
+
+});
+
+
+
+
+Route::get('/no-autorizado', function(){
+    return response()->json([
+        'message' => 'No autorizado, require token de seguridad',
+        'error' => '401'
+    ], 401);
+})->name('login');
